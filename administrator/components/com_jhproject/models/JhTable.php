9db42b41b7f915eef64169285089cd3a92d4fc93
vxdhost <vxdhost@8c00b02a-e956-11de-91d6-89caad0352f8>
2009-12-15 14:13:57 +0000

    Added base logic for FAQ system

--- administrator/components/com_jhproject/models/JhTable.php
+++ administrator/components/com_jhproject/models/JhTable.php
@@ -3,17 +3,38 @@
 Class JhTable extends JTable
 {
 
+	/**
+	 * @name __construct
+	 * @access public
+	 * @details constuctor class, calls JTable::_Construct with the table name, table key and database object
+	 * @return null
+	 */
 	function __construct()
 	{
 		$db	=& JFactory::getDBO();
 		parent::__construct( $this->_tbl, $this->_tbl_key, $db );
 	}
 	
+	/**
+	 * @name fetchRow
+	 * @access public
+	 * @details alias to JTable::load
+	 * @param $oid int/null
+	 * @return array
+	 */
 	function fetchRow($oid = null)
 	{
 		return parent::load( $oid);
 	}
 	
+	/**
+	 * @name fetchOne
+	 * @access public
+	 * @details alias to JTable::loadResult
+	 * @param $field text
+	 * @param $oid int/null
+	 * @return $field on success false of failure
+	 */
 	function fetchOne($field,$oid = null)
 	{
 		$k = $this->_tbl_key;
@@ -42,6 +63,16 @@ Class JhTable extends JTable
 		}
 	}
 	
+	/**
+	 * @name fetchAll
+	 * @access public
+	 * @details Fetch all results based on query
+	 * @param $where text/null
+	 * @param $order text/null
+	 * @param $limit int
+	 * @param $limitstart int
+	 * @return Array / object
+	 */
 	function fetchAll($where = null,$order = null, $limit = 0, $limitstart = 0)
 	{
 		$db =& $this->getDBO();
@@ -72,6 +103,13 @@ Class JhTable extends JTable
 		
 	}
 	
+	/**
+	 * @name fetchCount
+	 * @access public
+	 * @details returns count of rows
+	 * @param $where text/null
+	 * @return int on success false on failure
+	 */
 	function fetchCount($where = null)
 	{
 		$query = "Select count(*) as cnted from `" . $this->_tbl . "` ";
@@ -89,11 +127,25 @@ Class JhTable extends JTable
 		}
 	}
 	
+	/**
+	 * @name insert
+	 * @access public
+	 * @details alias to JTable::store
+	 * @param none
+	 * @return int
+	 */
 	function insert()
 	{
 		return $this->store();
 	}
 	
+	/**
+	 * @name query
+	 * @access public 
+	 * @details custom query
+	 * @param $query
+	 * @return dbQuery
+	 */
 	function query($query)
 	{
 		$db =& $this->getDBO();
@@ -101,6 +153,13 @@ Class JhTable extends JTable
 		return $db->query();
 	}
 	
+	/**
+	 * @name deleteList
+	 * @access public
+	 * @details Delete 1 or more items from the table
+	 * @param $oid, null/int/array
+	 * @return bool
+	 */
 	function deleteList( $oid=null )
 	{
 		$k = $this->_tbl_key;
@@ -120,5 +179,45 @@ Class JhTable extends JTable
 			$this->setError($this->_db->getErrorMsg());
 			return false;
 		}
+	}
+	
+	/**
+	 * @name fetchKeyArray
+	 * @access public
+	 * @details fetch array with specified index
+	 * @param $key text
+	 * @param $value array
+	 * @param $where text
+	 * @return Array
+	 */
+	function fetchKeyArray($key,$value,$where='')
+	{
+		$db =& $this->getDBO();
+		$db->setQuery("select `$key`,`".implode("`,`",$value)."` from `{$this->_tbl}` {$where} order by `$key` asc");
+		$result = $db->loadAssocList($key);
+		
+		return $result;
+	}
+	
+	/**
+	 * @name fetchKeyPair
+	 * @access public
+	 * @details fetch Indexed Array based on $key and $value
+	 * @param $key text
+	 * @param $value text
+	 * @param $where text
+	 * @return array
+	 */
+	function fetchKeyPair($key,$value,$where='')
+	{
+		$db =& $this->getDBO();
+		$db->setQuery("select `$key`,`$value` from `{$this->_tbl}` {$where} order by `$key` asc");
+		$result = $db->loadAssocList();
+		$return = array();
+		foreach($result as $res)
+			$return[$res[$key]] = $res[$value];
+		
+		return $return;
+	
 	}
 }
